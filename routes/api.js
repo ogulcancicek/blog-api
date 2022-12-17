226ee25ae831295c4529c03c0d791b48605c489a
Oğulcan Çiçek <57576342+ogulcancicek@users.noreply.github.com>
2022-12-18 02:12:45 +0300

    comment - routes

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
+const commentController = require('../controllers/commentController');
 
 router.get('/', (req, res) => {
     res.redirect('/api/posts');
@@ -25,6 +26,28 @@ router.put('/posts/:id',
             postController.update_post);
 // Get Single Post
 router.get('/posts/:id', postController.get_single_post);
+// Get Post Comments
+router.get('/posts/:postId/comments', commentController.get_comments);
+// Create Comment
+router.post('/posts/:postId/comments', 
+            passport.authenticate('jwt', { session: false }),
+            commentController.create_comment);
+// Update Comment
+router.put('/posts/:postId/comments/:commentId', 
+            passport.authenticate('jwt', { session: false }),
+            commentController.update_comment);
+// Delete Post Comments
+router.delete('/posts/:postId/comments', 
+            passport.authenticate('jwt', { session: false }),
+            commentController.delete_post_comments);
+// Delete Comment
+router.delete('/posts/:postId/comments/:commentId', 
+            passport.authenticate('jwt', { session: false }),
+            commentController.delete_single_comment);
+// Get Single Comment
+router.get('/posts/:postId/comments/:commentId', 
+            passport.authenticate('jwt', { session: false }),
+            commentController.get_single_comment);
 
 router.post('/sign-up', userController.signup);
 
