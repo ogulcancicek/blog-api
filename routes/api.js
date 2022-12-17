96491e05529241baec959f74382f9969d78836a9
Oğulcan Çiçek <57576342+ogulcancicek@users.noreply.github.com>
2022-12-18 01:37:09 +0300

    post - routes

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,29 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const userController = require('../controllers/userController');
+const postController = require('../controllers/postController');
 
 router.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Blog API.' });
+    res.redirect('/api/posts');
 });
+
+// Get All Posts
+router.get('/posts', postController.get_posts);
+
+// Create Post
+router.post('/posts',
+            passport.authenticate('jwt', { session: false }),
+            postController.create_post);
+// Delete Post
+router.delete('/posts/:id', 
+            passport.authenticate('jwt', { session: false }),
+            postController.delete_post);
+// Update Post
+router.put('/posts/:id',
+            passport.authenticate('jwt', { session: false }),
+            postController.update_post);
+// Get Single Post
+router.get('/posts/:id', postController.get_single_post);
 
 router.post('/sign-up', userController.signup);
 
