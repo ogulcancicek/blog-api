be99ac47c48057be95c63450c2618789df608577
Oğulcan Çiçek <57576342+ogulcancicek@users.noreply.github.com>
2022-12-18 12:59:00 +0300

    cors enabled

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
 const helmet = require('helmet');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -29,6 +30,7 @@ app.set('view engine', 'pug');
 
 app.use(helmet());
 app.use(compression());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
